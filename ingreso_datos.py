ba8586fed683b64d95d1873c88625c91c1819c35
davinsonm <davinsonminsuaste@outlook.com>
2019-10-01 19:15:46 -0500

    Add files via upload

---
+++ ingreso_datos.py
@@ -0,0 +1,8 @@
+def main():
+    print("ingrese nombres:"); Nombres = input()
+    print("ingrese apellidos:"); Apellidos = input()
+    print("ingrese Carrera:"); Carrera = input()
+    print("ingrese Facultad"); Facultad = input()
+    print("Ingrese Hobbie:"); Hobbie = input()
+if __name__=="__main__":
+    main()
